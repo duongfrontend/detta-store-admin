4b342bd80d7dfe352c8fd3bb24a5a00ca718dd13
duongfrontend <phuongnguyen12a6@gmail.com>
2024-03-17 15:21:45 +0700

    detta-updates

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -26,8 +26,8 @@ export async function POST(req: NextRequest) {
         allowed_countries: ["US", "CA", "VN"],
       },
       shipping_options: [
-        { shipping_rate: "shr_1OuVwaI6Qy8to7hU460HRQTB" },
-        { shipping_rate: "shr_1OuVvDI6Qy8to7hU5OegUK1E" },
+        { shipping_rate: "shr_1OvExFI6Qy8to7hU3o6uEXbD" },
+        { shipping_rate: "shr_1OvExsI6Qy8to7hU34vL25IO" },
       ],
       line_items: cartItems.map((cartItem: any) => ({
         price_data: {
